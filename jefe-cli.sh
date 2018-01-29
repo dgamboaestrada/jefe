8c0f7606e0ca27a89d38d729d3800ba5b0c391c1
Daniel Gamboa Estrada <dgamboaestrada@gmail.com>
2018-01-28 20:18:11 -0600

    Change docker commands for docker-compose and add network jefe-cli

--- jefe-cli.sh
+++ jefe-cli.sh
@@ -209,17 +209,6 @@ remove_vhost(){
     puts "Done." GREEN
 }
 
-# Remove jefe_nginx_proxy container.
-remove_nginx_proxy(){
-    # If jefe_nginx_proxy containr is running then stop
-    if [ "$(docker ps | grep jefe_nginx_proxy)" ]; then
-        stop_nginx_proxy
-    fi
-    puts "Removing jefe_nginx_proxy container..." BLUE
-    docker rm jefe_nginx_proxy
-    puts "Done." GREEN
-}
-
 # Fix permisions of the proyect folder
 fix_permisions(){
     load_dotenv
@@ -230,12 +219,17 @@ fix_permisions(){
     puts "Done." GREEN
 }
 
-# Create or start jefe_nginx_proxy container.
+# Create or start nginx_proxy container.
 start_nginx_proxy(){
+    if [ ! "$(docker network ls | grep jefe-cli)" ]; then
+        puts "Creating jefe-cli network..." BLUE
+        docker network create "jefe-cli"
+        puts "Done." GREEN
+    fi
     # If jefe_nginx_proxy containr not exist then create
     if [ ! "$(docker ps -a | grep jefe_nginx_proxy)" ]; then
         puts "Running jefe_nginx_proxy container..." BLUE
-        docker run -d --name jefe_nginx_proxy -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy:latest
+        docker run -d --name jefe_nginx_proxy -p 80:80 --network jefe-cli -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy:latest
         puts "Done." GREEN
     else
         puts "Starting jefe_nginx_proxy container..." BLUE
@@ -248,6 +242,17 @@ start_nginx_proxy(){
 stop_nginx_proxy(){
     puts "Stoping jefe_nginx_proxy container..." BLUE
     docker stop jefe_nginx_proxy
+    puts "Done." GREEN
+}
+
+# Remove jefe_nginx_proxy container.
+remove_nginx_proxy(){
+    # If jefe_nginx_proxy containr is running then stop
+    if [ "$(docker ps | grep jefe_nginx_proxy)" ]; then
+        stop_nginx_proxy
+    fi
+    puts "Removing jefe_nginx_proxy container..." BLUE
+    docker rm jefe_nginx_proxy
     puts "Done." GREEN
 }
 
@@ -270,7 +275,7 @@ up() {
 up [-d] [--deleted-mode] [-p] [--production] [-h] [--help]
 
 Arguments:
-    -d, --deleted-mode		Detached mode: Run containers in the background
+    -d, --detached-mode		Detached mode: Run containers in the background
     -p, --production		Run containers with production configuration
     -h, --help			Print Help (this message) and exit
 EOF
@@ -294,6 +299,7 @@ EOF
         esac
     done
 
+    start_nginx_proxy
     set_vhost
     load_dotenv
     cd .jefe/
@@ -327,7 +333,7 @@ restart() {
 # Stop and remove containers, networks, images, and volumes.
 down() {
     usage= cat <<EOF
-ps [-v <option>] [--volumes <option>] [-h] [--help]
+jefe down [-v <option>] [--volumes <option>] [-h] [--help]
 
 Arguments:
     -v, --volumes		Remove volumes of the proyect. Options force, not_force.
